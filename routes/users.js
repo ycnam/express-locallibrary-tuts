8e842b45f8d5e12039606a04fedf2f2e17df8d54
Youngchul Nam <limitist@gmail.com>
2021-04-20 00:53:12 +0900

    add ycnam profile page

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,13 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+  res.send("respond with a resource");
+});
+
+router.get("/ycnam", function (req, res, next) {
+  res.send("welcome to youngchul's page!");
 });
 
 module.exports = router;
